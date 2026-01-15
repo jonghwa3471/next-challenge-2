ac0b0bb3217b7e6a3c71b35f29bc9e2e7db90815
Jonghwa <whdghk3471@gmail.com>
2026-01-15 22:48:29 +0900

    add: prisma-adapter-pg

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,18 @@
 import { PrismaClient } from "./generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL,
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
 });
 
-const db = new PrismaClient({ adapter });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
 
 export default db;
